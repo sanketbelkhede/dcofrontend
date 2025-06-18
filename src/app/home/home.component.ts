96d8920da2e99132ed5da48701566adbd594d027
Sanket Belkhede <sanketbelkhede01.com>
2025-06-18 17:02:48 +0530

    home component done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import {Router} from "@angular/router";
-import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    NgOptimizedImage
-  ],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
