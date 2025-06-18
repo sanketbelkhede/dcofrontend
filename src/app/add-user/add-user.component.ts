11ff514bbfba81f9befb4a76ac2fd0619a9a5f61
Sanket Belkhede <sanketbelkhede01.com>
2025-06-18 20:47:05 +0530

    add user screen done

--- src/app/add-user/add-user.component.ts
+++ src/app/add-user/add-user.component.ts
@@ -1,27 +1,42 @@
 import { Component, OnInit} from '@angular/core';
 import {RestService} from "../services/rest-service";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
+import {ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup, Validators} from "@angular/forms";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatError } from '@angular/material/form-field';
+import {MessageService} from "../services/message.service"; // for mat-error
+
 
 @Component({
   selector: 'app-add-user',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    ReactiveFormsModule,
+    NgIf,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatError
   ],
   templateUrl: './add-user.component.html',
   styleUrl: './add-user.component.css'
 })
 export class AddUserComponent implements OnInit{
-
   countries: any = [];
   states: any = [];
 
-  constructor(private restService: RestService) {
-  }
+  userForm!: UntypedFormGroup;
+
+  constructor(private formBuilder: UntypedFormBuilder, private restService: RestService, private messageService: MessageService) {}
 
   ngOnInit(): void {
-    this.getCountries();
-    this.getStates();
+    this.userForm = this.formBuilder.group({
+      userEmail: ['', [Validators.required, Validators.email]],
+      userPhone: ['', [Validators.required, Validators.pattern("[0-9]{10}")]],
+      userPanCard: ['', [Validators.required, Validators.pattern("[A-Z]{5}[0-9]{4}[A-Z]{1}")]]
+    });
   }
 
   getCountries() {
@@ -34,5 +49,16 @@ export class AddUserComponent implements OnInit{
     this.restService.getStates().subscribe(states => {
       this.states = states;
     });
+  }
+
+  submit() {
+    this.restService.verifyUser(this.userForm.value).subscribe((reply: any) => {
+      this.messageService.closableSnackBar(reply.message);
+    })
+  }
+
+  reset() {
+    console.log("reset")
+    this.userForm.reset();
   }
 }
